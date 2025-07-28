cb869cfeb12ce5117254b7352a694f89c5b88a50
Swastik2005 <swastikdevil123@gmail.com>
2025-07-28 17:52:08 +0530

    new change

--- components/auth/Details.jsx
+++ components/auth/Details.jsx
@@ -116,7 +116,7 @@ const Details=()=>{
 
   if(loading) {
     return (
-      <div className="flex justify-center items-center h-full">
+      <div className="flex-1/3 justify-center items-center h-full">
         <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-blue-500"></div>
       </div>
     );
